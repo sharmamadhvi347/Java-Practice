0d6ede2036c3b6512e33ea2580db38a562762771
Madhvi Sharma <sharmamadhvi347@gmail.com>
2025-08-14 14:15:37 +0530

    strlen

---
+++ Week 1-S1-LabProblem/strlen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class strlen{
+		public static int getStringlength(String str){
+			int count=0;
+			try{
+				while(true){
+					str.charAt(count);
+					count++;
+				}
+			}catch(IndexOutOfBoundsException e){
+			}
+			return count;
+		}
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Enter a string:");
+			String input = scanner.next();
+			int newlength = getStringlength(input);
+			int builtInLength=input.length();
+			System.out.println("calculate length:"+newlength);
+			System.out.println("calculate length using length():"+builtInLength);
+		}
+}
+
